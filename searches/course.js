1d1bd1f356eb93488bb2af4e64ee421e77e7df83
Ray Hatfield <ray.hatfield@gmail.com>
2021-09-22 15:27:57 -0500

    use CourseDetails info in course search

--- searches/course.js
+++ searches/course.js
@@ -1,6 +1,10 @@
 const { fetchLink } = require('../utils/workspace.js');
+const CourseDetails = require('../lib/event-data/CourseDetails');
 
 // https://github.com/NextThought/nti.app.products.zapier/blob/master/docs/initial_api.rst#search-course
+
+const {transform, outputFields} = CourseDetails;
+const sample = CourseDetails.sample.output();
 
 module.exports = {
     key: 'course',
@@ -23,34 +27,10 @@ module.exports = {
 
         perform: async (z, bundle) => {
             const response = await fetchLink('course_search', z, bundle);
-            z.console.log(response.data);
-            return response.data.Items;
+            return response.data.Items.map(transform);
         },
 
-        // In cases where Zapier needs to show an example record to the user, but we are unable to get a live example
-        // from the API, Zapier will fallback to this hard-coded sample. It should reflect the data structure of
-        // returned records, and have obviously dummy values that we can show to any user.
-        sample: {
-            'Id': 'tag:nextthought.com,2011-10:NTI-CourseInfo-0000000000000000000_0000000000000000000',
-            'Title': 'Zapier Test Course 001',
-            'ProviderId': 'ZT-001',
-            'Description': '',
-            'RichDescription': 'Testing zapier trigger',
-            'StartDate': '2021-08-25T19:32:59Z',
-            'EndDate': '2021-08-25T19:32:59Z',
-            'CreatedTime': '2021-08-25T19:32:59Z',
-            'Last Modified': '2021-08-25T19:32:59Z',
-        },
-
-        outputFields: [
-            { key: 'Id', label: 'Course ID' },
-            { key: 'CreatedTime', label: 'Created Time' },
-            { key: 'ProviderId', label: 'Provider ID' },
-            { key: 'Title' },
-            { key: 'Description' },
-            { key: 'RichDescription', label: 'Description (Rich Text)' },
-            { key: 'StartDate', label: 'Start Date' },
-            { key: 'EndDate', label: 'End Date' },
-        ]
+        sample,
+        outputFields,
     },
 };
